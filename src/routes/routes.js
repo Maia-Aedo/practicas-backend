0dd9aa5dd8ff71513449ed2ab62c905e2da2542b
Maia-Aedo <aedomaia10@gmail.com>
2024-09-09 20:10:26 -0300

    routes

---
+++ src/routes/routes.js
@@ -0,0 +1,20 @@
+// router para usar metodos POST-PUT-GET-DELETE-PATCH
+const { Router } = require('express');
+// metodos de controlador
+const methods = require('../controllers/users.controller');
+// autenticacion
+const { authenticateJWT } = require('../middlewares/jwt');
+
+// instancia router
+const router = Router();
+
+// configuramos cadaa endpoint con su metodo;
+router.post('/users/register', methods.register);
+router.post('/users/login', methods.login);
+// :id es un path parameter, express lo divide y coloca en req.params
+/* colocamos el middleware para evitar que cualquiera que consulte el endpoint vea el token
+que solo se puede conseguir mediante autenticacion en login */
+router.get('/users/:id', authenticateJWT, methods.getOne);
+
+// exportamos router
+module.exports = router;
